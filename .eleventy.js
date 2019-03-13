577ab466629b5662197d6568c40a0fca91f1fc97
Andrew Hudson <ahudson@gmail.com>
2019-03-13 17:27:31 +0000

    finish filter for showing current posts only not future posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,28 @@
-const { DateTime } = require('luxon');
-const fs = require('fs');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
-const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const { DateTime } = require("luxon");
+const fs = require("fs");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-  eleventyConfig.addFilter('readableDate', dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('dd-LL-yyyy');
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    console.log(dateObj);
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("dd-LL-yyyy");
   });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  eleventyConfig.addFilter("htmlDateString", dateObj => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
 
   // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter('head', (array, n) => {
-    if ( n < 0 ) {
+  eleventyConfig.addFilter("head", (array, n) => {
+    if (n < 0) {
       return array.slice(n);
     }
 
@@ -29,23 +30,30 @@ module.exports = function(eleventyConfig) {
   });
 
   // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter('past', (array) => {
-    array.forEach(post => {
-      console.log(post.data.date);
+  eleventyConfig.addFilter("past", array => {
+    const todaysDate = Date.now().toString();
 
+    let retArray = array.filter(post => {
+      const postDate = DateTime.fromJSDate(post.data.date, {
+        zone: "utc"
+      }).toFormat("x");
 
-    })
-    return array;
+      if (postDate <= todaysDate) {
+        return post;
+      }
+    });
+
+    return retArray;
   });
 
-  eleventyConfig.addCollection('tagList', require('./_11ty/getTagList'));
+  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
 
-  eleventyConfig.addPassthroughCopy('img');
-  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("css");
 
   /* Markdown Plugins */
-  let markdownIt = require('markdown-it');
-  let markdownItAnchor = require('markdown-it-anchor');
+  let markdownIt = require("markdown-it");
+  let markdownItAnchor = require("markdown-it-anchor");
   let options = {
     html: true,
     breaks: true,
@@ -53,20 +61,21 @@ module.exports = function(eleventyConfig) {
   };
   let opts = {
     permalink: true,
-    permalinkClass: 'direct-link',
-    permalinkSymbol: '#'
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#"
   };
 
-  eleventyConfig.setLibrary('md', markdownIt(options)
-    .use(markdownItAnchor, opts)
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt(options).use(markdownItAnchor, opts)
   );
 
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
       ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html');
+        const content_404 = fs.readFileSync("_site/404.html");
 
-        browserSync.addMiddleware('*', (req, res) => {
+        browserSync.addMiddleware("*", (req, res) => {
           // Provides the 404 content without redirect.
           res.write(content_404);
           res.end();
@@ -76,28 +85,23 @@ module.exports = function(eleventyConfig) {
   });
 
   return {
-    templateFormats: [
-      'md',
-      'njk',
-      'html',
-      'liquid'
-    ],
+    templateFormats: ["md", "njk", "html", "liquid"],
 
     // If your site lives in a different subdirectory, change this.
     // Leading or trailing slashes are all normalized away, so don’t worry about it.
     // If you don’t have a subdirectory, use ' or '/' (they do the same thing)
     // This is only used for URLs (it does not affect your file structure)
-    pathPrefix: '/',
+    pathPrefix: "/",
 
-    markdownTemplateEngine: 'liquid',
-    htmlTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
     passthroughFileCopy: true,
     dir: {
-      input: '.',
-      includes: '_includes',
-      data: '_data',
-      output: '_site'
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
     }
   };
 };
