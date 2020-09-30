28ba1182f8e3b962c5db093af45d15bd492623a1
Andrew Hudson <andrew@ventureharbour.com>
2020-09-30 16:03:50 +0100

    pass isProduction from _data to output service-worker file

---
+++ src/_data/metadata.js
@@ -0,0 +1,13 @@
+module.exports = {
+	title: 'Andrew JD Hudson',
+	siteTitle: 'Andrew JD Hudson',
+	url: 'https://andrewhudson.dev/',
+	description: "Andrew JD Hudson's front-end development blog",
+	shortname: 'AH',
+	author: {
+		name: 'Andrew JD Hudson',
+		email: 'ahudson@gmail.com',
+	},
+	themeColor: '#ffcc00',
+	isProduction: process.env.ELEVENTY_ENV === 'production',
+};
