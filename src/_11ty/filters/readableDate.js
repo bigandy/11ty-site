cb492567d6c7defb894ab2a152da092605c97a06
Andrew Hudson <andrew@ventureharbour.com>
2020-11-03 10:54:59 +0000

    Some CSS fixing and tweaking. Change readableDate format

--- src/_11ty/filters/readableDate.js
+++ src/_11ty/filters/readableDate.js
@@ -1,7 +1,36 @@
 const { DateTime } = require('luxon');
 
-const readableDate = dateObj => {
-	return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('dd-LL-yyyy');
+const getNumberSuffix = (num) => {
+	const th = 'th';
+	const rd = 'rd';
+	const nd = 'nd';
+	const st = 'st';
+
+	if (num === 11 || num === 12 || num === 13) return th;
+
+	const lastDigit = num.toString().slice(-1);
+
+	switch (lastDigit) {
+		case '1':
+			return 'st';
+		case '2':
+			return 'nd';
+		case '3':
+			return 'rd';
+		default:
+			return 'th';
+	}
+};
+
+const readableDate = (dateObj) => {
+	const formatDate = DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+		'd-LLLL-yyyy'
+	);
+
+	const [day, month, year] = formatDate.split('-');
+	const outputDay = `${day}${getNumberSuffix(day)}`;
+
+	return `${outputDay} ${month} ${year}`;
 };
 
 module.exports = readableDate;
