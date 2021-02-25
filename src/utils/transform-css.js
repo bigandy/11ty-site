5e4092ba445770fb50bbed70252e284a2e65a23c
Andrew Hudson <andrew@ventureharbour.com>
2021-02-25 20:43:13 +0000

    update deps

--- src/utils/transform-css.js
+++ src/utils/transform-css.js
@@ -2,6 +2,12 @@ const path = require('path');
 const postcss = require('postcss');
 const fs = require('fs').promises;
 
+const atImport = require('postcss-import');
+const precss = require('precss');
+const postcssCustomSelectors = require('postcss-custom-selectors');
+const cssNano = require('cssnano');
+const postCssNested = require('postcss-nested');
+
 module.exports = async (filename) => {
 	const rawFilepath = path.join(
 		__dirname,
@@ -9,14 +15,19 @@ module.exports = async (filename) => {
 	);
 	const code = await fs.readFile(rawFilepath);
 
-	return await postcss([
-		require('precss'),
-		require('postcss-import'),
-		require('postcss-custom-selectors'),
-		// require('autoprefixer'),
-		require('cssnano'),
+	const result = await postcss([
+		precss,
+		atImport,
+		postcssCustomSelectors,
+		postCssNested,
+		cssNano,
 	])
 		.process(code, { from: rawFilepath })
-		.then((result) => result.css)
+		.then((result) => {
+			// console.log(result);
+			return result.css;
+		})
 		.catch((e) => console.log('error', e));
+
+	return result;
 };
