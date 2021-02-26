dbd7e9a4c44a22cc22bc40b897b9cc1a7c183fe6
Andrew Hudson <andrew@ventureharbour.com>
2021-02-26 09:25:53 +0000

    try updating the npmrc to change node version

--- src/utils/transform-css.js
+++ src/utils/transform-css.js
@@ -2,12 +2,6 @@ const path = require('path');
 const postcss = require('postcss');
 const fs = require('fs').promises;
 
-const atImport = require('postcss-import');
-const precss = require('precss');
-const postcssCustomSelectors = require('postcss-custom-selectors');
-const cssNano = require('cssnano');
-const postCssNested = require('postcss-nested');
-
 module.exports = async (filename) => {
 	const rawFilepath = path.join(
 		__dirname,
@@ -15,19 +9,14 @@ module.exports = async (filename) => {
 	);
 	const code = await fs.readFile(rawFilepath);
 
-	const result = await postcss([
-		precss,
-		atImport,
-		postcssCustomSelectors,
-		postCssNested,
-		cssNano,
+	return await postcss([
+		require('precss'),
+		require('postcss-import'),
+		require('postcss-custom-selectors'),
+		// require('autoprefixer'),
+		require('cssnano'),
 	])
 		.process(code, { from: rawFilepath })
-		.then((result) => {
-			// console.log(result);
-			return result.css;
-		})
+		.then((result) => result.css)
 		.catch((e) => console.log('error', e));
-
-	return result;
 };
