f0dc79741c87ca50ac9796524d87a4ea8ccf1ad3
Andrew Hudson <ahudson@scalable.com>
2024-05-14 21:54:57 +0100

    formatting

--- functions/micropub.mjs
+++ functions/micropub.mjs
@@ -45,9 +45,9 @@ export default async (req) => {
 	const filename = slugify(getURLDate(date));
 	// Convert date and content into Markdown template
 	const template = `---
-	date: ${date.toISOString()}
-	---
-	${decodeURIComponent(content)}`;
+date: ${date.toISOString()}
+---
+${decodeURIComponent(content)}`;
 	// Create files in repo
 
 	return await octokit.repos
